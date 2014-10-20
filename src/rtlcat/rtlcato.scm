20564e613a8d5442faa7ef3bac879e35cc2bcab0
Brandon Bloom <brandon@brandonbloom.name>
2014-10-20 01:48:39 -0400

    it runs backwards!

--- src/rtlcat/rtlcato.scm
+++ src/rtlcat/rtlcato.scm
@@ -9,48 +9,98 @@
          (== `(,a . ,res) out)
          (appendo d s res))))))
 
+(define seqo
+  (lambda (out)
+    (fresh (a d)
+      (conde
+        ((== '() out) succeed)
+        ((== `(,a . ,d) out) succeed)
+        (succeed fail)))))
+
+(define quotationo seqo)
+
+(define literalo
+  (lambda (out)
+    (conde
+      ((numbero out) succeed)
+      ((quotationo out) succeed))))
+
 (define cato
-  (lambda (program o)
+  (lambda (program out)
     (conde
 
-      ((== '() program) (== '() o))
+      ((== '() program) (== '() out))
 
       ((fresh (word args)
          (== `(,word . ,args) program)
          (conde
 
-            ;; literal numbers
-            ((numbero word)
+            ;; literals
+            ((literalo word)
              (fresh (ret)
                (cato args ret)
-               (== `(,word . ,ret) o)))
+               (== `(,word . ,ret) out)))
 
-            ;; swap (a b -- b a)
+            ;; swap ( a b -- b a )
             ((== 'swap word)
              (fresh (a b k ret)
                (cato args `(,a ,b . ,k))
                (cato k ret)
-               (== `(,b ,a . ,ret) o)))
+               (== `(,b ,a . ,ret) out)))
 
-             ;; dup (a -- a a)
+             ;; dup ( a -- a a )
              ((== 'dup word)
               (fresh (a k ret)
                 (cato args `(,a . ,k))
                 (cato k ret)
-                (== `(,a ,a . ,ret) o)))
+                (== `(,a ,a . ,ret) out)))
 
-             ;; drop (a -- )
+             ;; drop ( a -- )
              ((== 'drop word)
               (fresh (a k ret)
                 (cato args `(,a . ,k))
-                (cato k o)))
+                (cato k out)))
+
+             ;; call ( q -- )
+             ((== 'call word)
+              (fresh (q k qk ret)
+                (cato args `(,q . ,k))
+                (appendo q k qk)
+                (cato qk out)))
+
+             ;; cons ( x lst -- newlst )
+             ((== 'cons word)
+              (fresh (x lst newlst k ret)
+                (cato args `(,x ,lst . ,k))
+                (== `(,x . ,lst) newlst)
+                (cato k ret)
+                (cato `(,newlst . ,ret) out)))
+
+             ;; dip ( x q -- x )
+             ((== 'dip word)
+              (fresh (q x k qk ret)
+                (cato args `(,q ,x . ,k))
+                (appendo `(,x . ,q) k qk)
+                (cato qk out)))
+
 
              ))))))
 
 #|
 
+;; forwards
+(run 3 (q) (cato '() q))
+(run 3 (q) (cato '(1) q))
+(run 3 (q) (cato '(1 (2 3) 4) q))
+(run 3 (q) (cato '(1 swap 2 3) q))
+(run 5 (q) (cato '(call (dup) 5) q))
+(run 5 (q) (cato '(cons 1 (2 3)) q))
+(run 5 (q) (cato '(dip (swap) 1 3 5) q))
+
+;; BACKWARDS!
 (run 5 (q) (cato q '(1 1)))
 
-(run 1 (q) (cato q q))
+;; quines!?
+(run 20 (q) (cato q q))
 
 |#
