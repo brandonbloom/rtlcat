3382b8a9d50a804e7f95f27d3025cd8d6405969e
Brandon Bloom <brandon@brandonbloom.name>
2014-10-19 21:32:25 -0400

    lazy version

--- src/rtlcat/core.clj
+++ src/rtlcat/core.clj
@@ -3,30 +3,28 @@
 (defn literal? [x]
   (number? x))
 
-(defn run [program]
-  (when-first [word program]
-    (cond
-      (literal? word) (cons word (run (next program)))
-      (fn? word) (word program)
-      :else (throw (Exception. (str "Bad value: " (pr-str word)))))))
+(defn word? [x]
+  (fn? x))
 
-(defn split-arg [program]
-  (when (seq program)
-    (let [[x & xs] program]
-      (if (literal? x)
-        [x xs]
-        (when-let [[y & ys :as ret] (run xs)]
-          [y ys])))))
+(defn run [program]
+  (when-let [[x & xs] (seq program)]
+    (cond
+      (literal? x) (cons x (run xs))
+      (word? x) (x xs)
+      :else (throw (Exception. (str "bad value: " (pr-str x)))))))
 
 (defn add [program]
-  (if-let [[x program] (split-arg program)]
-    (if-let [[y program] (split-arg program)]
-      (cons (+ x y) (run program))
-      (throw (Exception. "Underflow")))
-    (throw (Exception. "Underflow"))))
+  (when-let [[x & program] (run program)]
+    (when-let [[y & program] (run program)]
+      (lazy-seq
+        (cons (+ x y) program)))))
 
 (comment
 
+  (run [])
+  (run [1])
+  (run [1 2 3])
+  (run [add 1 2])
   (run [1 2 add add 3 4 5])
 
 )
