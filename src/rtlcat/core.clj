a7e21b96445d0a6a00ff42077ad0698745fada1c
Brandon Bloom <brandon@brandonbloom.name>
2014-10-19 21:59:37 -0400

    swap, drop, call, dip, etc

--- src/rtlcat/core.clj
+++ src/rtlcat/core.clj
@@ -1,7 +1,12 @@
-(ns rtlcat.core)
+(ns rtlcat.core
+  (:refer-clojure :exclude [drop]))
+
+(defn quotation? [x]
+  (vector? x))
 
 (defn literal? [x]
-  (number? x))
+  (or (number? x)
+      (quotation? x)))
 
 (defn word? [x]
   (fn? x))
@@ -18,6 +23,27 @@
     (when-let [[y & program] (run program)]
       (cons (+ x y) program))))
 
+(defn dup [program]
+  (when-let [[x & program] (run program)]
+    (cons x (cons x program))))
+
+(defn swap [program]
+  (when-let [[x & program] (run program)]
+    (when-let [[y & program] (run program)]
+      (cons y (cons x program)))))
+
+(defn drop [program]
+  (next (run program)))
+
+(defn call [program]
+  (when-let [[q & program] (run program)]
+    (run (concat q program))))
+
+(defn dip [program]
+  (when-let [[q & program] (run program)]
+    (when-let [[hide & program] (run program)]
+      (run (concat [hide] q program)))))
+
 (comment
 
   (run [])
@@ -25,5 +51,11 @@
   (run [1 2 3])
   (run [add 1 2])
   (run [1 2 add add 3 4 5])
+  (run [add 1]) ; underflows
+  (run [add dup 1])
+  (run [swap 5 10])
+  (run [add call [dup] 3])
+  (run [3 dip [add] 5 7 9])
+  (run [5 drop 10 15])
 
 )
